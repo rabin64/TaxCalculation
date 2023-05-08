6c6acbf383173d2696c7e802d31339d49608a974
Rabin Shrestha <rabin.shrestha@amniltech.com>
2023-05-08 15:24:21 +0545

    changes class name

--- TaxCalculationConsole/Program.cs
+++ TaxCalculationConsole/Program.cs
@@ -4,15 +4,16 @@ using TaxCalculation;
 using System;
 
 public class Program
-{   
+{
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         CalculateTax calculateTax = new CalculateTax();
-       // double totalTax = calculateTax.CalculateAdditionalTotalTax(400, 1500);
+        // double totalTax = calculateTax.CalculateAdditionalTotalTax(400, 1500);
         double totalTax = calculateTax.CalculateAdditionalTotalTax(1100, 7000);
-        //double totalTax = calculateTax.CalculateAdditionalTotalTax(5000, 3000);
+        //double totalTax = calculateTax.CalculateAdditionalTotalTax(10, 5000);
 
         Console.WriteLine($"Total Tax: {totalTax:F2}");
     }
 }
+    
